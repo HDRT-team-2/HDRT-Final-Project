4a5bab5f33be74b1e57d14624d144d6535bd5295
netasha <drfxsw@gmail.com>
2025-10-22 17:33:30 +0900

    feat: #43 카드 스타일 적용

--- frontend/battlefield-monitor/src/components/common/Card.vue
+++ frontend/battlefield-monitor/src/components/common/Card.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 defineProps<{ title: string }>()
-
 </script>
-<template>
-  <div>
-    <div class="card-header">{{ title }}</div>
-    <div class="card-body"><slot /></div>
-  </div>
 
+<template>
+  <div class="border border-gray-300 rounded-lg shadow-sm hover:shadow-md m-1">
+    <!-- Header -->
+    <div class="bg-military-300 border-b border-gray-200 px-4 py-3">
+      <h3 class="text-sm font-bold text-gray-800 m-0 tracking-wide">{{ title }}</h3>
+    </div>
+    
+    <!-- Body -->
+    <div class="p-4 overflow-y-auto">
+      <slot />
+    </div>
+  </div>
 </template>
