0dcb5fddaba05ba86e848f9ecfbca956cee41f5c
netasha <drfxsw@gmail.com>
2025-11-15 17:26:27 +0900

    feat: #210 header 디자인변경 및 로고, 시간추가

--- frontend/TCIS/src/layouts/AppHeader.vue
+++ frontend/TCIS/src/layouts/AppHeader.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import SendBtn from '@/components/common/SendBtn.vue';
+import logo from '@/components/icons/HRLogo.vue'
+import Time from '@/components/common/Time.vue'
 </script>
 <template>
-  <header class="bg-rotem-600 flex items-center justify-center">
-    <h1 class="text-white text-2xl">Battlefield Monitor</h1>
-    <SendBtn />
-    <!-- <TestBtn /> -->
+  <header class="bg-gradient-to-r from-rotem-800 to-rotem-600 flex items-center p-2">
+    <logo />
+    <h1 class="text-white text-2xl flex-1 text-center">
+      Tactical Command Information System
+    </h1>
+    <Time />
+    <!-- <SendBtn /> -->
   </header>
 </template>
