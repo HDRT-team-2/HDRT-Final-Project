2e7ff8166443c3bb4958cbae1fcbf806c3a247d3
netasha <drfxsw@gmail.com>
2025-11-15 17:06:31 +0900

    feat: #210 카드 디자인 변경

--- frontend/TCIS/src/components/common/Card.vue
+++ frontend/TCIS/src/components/common/Card.vue
@@ -3,9 +3,9 @@ defineProps<{ title: string }>()
 </script>
 
 <template>
-  <div class="border border-rotem-100 rounded-lg shadow-sm hover:shadow-md w-full">
+  <div class="border border-rotem-100 rounded-lg shadow-sm shadow-md w-full">
     <!-- Header -->
-    <div class="bg-gradient-to-b from-rotem-600 to-rotem-500 border-b rounded-t-lg border-rotem-200 px-4 py-3">
+    <div class="bg-gradient-to-b from-rotem-800 to-rotem-600 border-b rounded-t-lg border-rotem-100 px-4 py-3">
       <h3 class="text-sm font-bold text-rotem-50 m-0 tracking-wide">{{ title }}</h3>
     </div>
     
